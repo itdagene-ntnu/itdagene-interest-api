609a9a8d20032e05a13a042bf90e0d49da794254
Peder Smith <smith.peder@gmail.com>
2019-06-25 00:54:53 +0200

    Fix mailtemplate after split

--- mailHandler.js
+++ mailHandler.js
@@ -1,14 +1,14 @@
-const nodemailer = require('nodemailer');
-const creds = require('./client_secret.json');
-const fs = require('fs');
-const path = require('path');
+const nodemailer = require("nodemailer");
+const creds = require("./client_secret.json");
+const fs = require("fs");
+const path = require("path");
 
 let transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    type: 'OAuth2',
+    type: "OAuth2",
     user: process.env.EMAIL,
     serviceClient: creds.client_id,
     privateKey: creds.private_key
@@ -16,15 +16,14 @@ let transporter = nodemailer.createTransport({
 });
 
 async function mailHandler(interest) {
-  fs.readFile(path.resolve(__dirname, '../public/mailTemplate'), function(
-    err,
-    html
-  ) {
+  fs.readFile(path.resolve(__dirname, "./mailTemplate"), function(err, html) {
     transporter.sendMail({
-      from: `"itDAGENE ${process.env.REACT_APP_YEAR} Interesse" <${process.env.EMAIL}>`,
-      to: `${interest.contactPerson} - ${interest.companyName} <${interest.contactEmail}>`,
-      subject: `itDAGENE ${process.env.REACT_APP_YEAR}: ${interest.companyName}`,
-      text: 'Bekreftelse Interesse itDAGENE',
+      from: `"itDAGENE ${process.env.YEAR} Interesse" <${process.env.EMAIL}>`,
+      to: `${interest.contactPerson} - ${interest.companyName} <${
+        interest.contactEmail
+      }>`,
+      subject: `itDAGENE ${process.env.YEAR}: ${interest.companyName}`,
+      text: "Bekreftelse Interesse itDAGENE",
       html: html
     });
   });
